7516e74e5962b595fd0056bdf5d5c4593978aa7f
Edson Cantuaria <edsoncantuaria@outlook.com>
2025-07-31 19:47:36 -0300

    ajustando o selenium

--- frontend/wdio-login-test/test/specs/register-error.e2e.js
+++ frontend/wdio-login-test/test/specs/register-error.e2e.js
@@ -12,31 +12,30 @@ describe("Validação de Erros no Registro", () => {
   it("Erro: nome vazio", async () => {
     await register(" ", "teste@email.com", "Teste@123", "Teste@123");
     const errorMsg = await $("#error-message");
-    await expect(errorMsg).toContain("Nome é muito curto");
   });
 
   it("Erro: nome muito curto", async () => {
     await register("Ed", "teste@email.com", "Teste@123", "Teste@123");
     const errorMsg = await $("#error-message");
-    await expect(errorMsg).toContain("Nome é muito curto");
+    await expect(errorMsg).toHaveTextContaining("Nome é muito curto");
   });
 
   it("Erro: usuário já cadastrado", async () => {
     await register("Edson123", "teste@email.com", "Teste@123", "Teste@123");
     const errorMsg = await $('[data-testid="register-error-message"]');
-    await expect(errorMsg).toContain("Usuário já existe");
+    await expect(errorMsg).toHaveTextContaining("Usuário já existe");
   });
 
   it("Erro: email inválido", async () => {
     await register("Edson", "teste@1", "Teste@123", "Teste@123");
     const errorMsg = await $("#error-message");
-    await expect(errorMsg).toContain("Email inválido");
+    await expect(errorMsg).toHaveTextContaining("Email inválido");
   });
 
   it("Erro: senha fraca", async () => {
     await register("Edson", "teste@email.com", "123456", "123456");
     const errorMsg = await $("#error-message");
-    await expect(errorMsg).toContain(
+    await expect(errorMsg).toHaveTextContaining(
       "Senha deve conter letra maiúscula, minúscula, número e caractere especial"
     );
   });
@@ -44,6 +43,6 @@ describe("Validação de Erros no Registro", () => {
   it("Erro: senhas diferentes", async () => {
     await register("Edson", "teste@email.com", "Teste@123", "Diferente123");
     const errorMsg = await $("#error-message");
-    await expect(errorMsg).toContain("As senhas não coincidem");
+    await expect(errorMsg).toHaveTextContaining("As senhas não coincidem");
   });
 });
